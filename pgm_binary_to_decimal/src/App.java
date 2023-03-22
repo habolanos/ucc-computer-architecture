5da9db38cbf51c225922be08c573dca41f5ac324
Harold Adrian <haroldb@mill5.com>
2023-03-21 19:34:14 -0500

    Source code from ValentinaSuarez0324

---
+++ pgm_binary_to_decimal/src/App.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner capturar = new Scanner (System.in);
+        System.out.println("Conversor Binario a Decimal");
+        System.out.println("-----------------------------");
+        System.out.println("Ingrese un número binario 📥");
+        System.out.println("-----------------------------");
+        String numero = capturar.next();
+        int exponente = 0;
+        double resultado = 0;
+        double operacion = 0;
+        String imprimir = "si";
+        
+        for (int i = numero.length()-1; i >= 0; i--){
+
+            char dato = numero.charAt(i);
+            String digito = String.valueOf(dato);
+
+            operacion = Math.pow(2, exponente);
+
+            if (digito.equals("1")){
+            resultado = resultado + operacion;
+            exponente ++;
+
+
+            } else if (digito.equals("0")) {
+                exponente ++;
+
+            } else {
+                imprimir = "no";
+                System.out.println("-----------------------------------------");
+                System.out.println("❌ El dato ingresado no es un numero binario ❌");
+                System.out.println("-----------------------------------------");
+            }
+                
+        }
+                if (imprimir.equals("si")){
+                    System.out.println("-----------------------------------------");
+                    System.out.println("✅El resultado es: " + resultado);
+                    System.out.println("-----------------------------------------");
+                }
+                capturar.close();
+                
+    }
+}
---
+++ pgm_decimal_to_binary/src/App.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Conversor de decimal a binario");
+        Scanner Capturar = new Scanner(System.in);
+        int dividendo = 0;
+        int divisor = 2;
+        int cociente = 0;
+        int residuo = 0;
+        String resultado = "";
+        String operar = "si";
+        String binario = "";
+        System.out.println("-------------------------------------");
+        System.out.println("Ingrese el número que desea convertir📥");
+        System.out.println("-------------------------------------");
+        System.out.print(">");
+        dividendo = Capturar.nextInt(); 
+        
+// Conversión
+        cociente=dividendo;
+        while(operar ==  "si"){
+            cociente = dividendo / divisor;
+            residuo = dividendo % divisor;
+
+            if (cociente >= 2) {
+
+            resultado = resultado + residuo;
+            dividendo = cociente;
+            } else {
+
+                resultado = resultado + residuo;
+                operar = "no";
+            } 
+                
+
+        } 
+// Ordena los números
+             resultado = resultado + cociente;
+                Capturar.close();
+                for (int i= resultado.length() -1; i > 0 -1  ; i--) {
+
+                    char dato = resultado.charAt(i);
+                    binario = binario + dato;
+                }
+                System.out.println("✅El resultado es " + binario);
+    }       
+
+
+} 
